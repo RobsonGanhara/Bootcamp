6947ace26ad0edb93197d8afcea2bf32207f3209
Robson Ganhara <robsongp@hotmail.com>
2024-12-06 10:13:58 -0300

    Exercícios 01 e 02 do Bootcamp

---
+++ aula01_bootcamp/exercicio01.py
@@ -0,0 +1,3 @@
+# Crie um programa que o usuário digite seu nome e retorne o número de caracteres
+
+print(len(input('Digite seu nome: ')))
---
+++ aula01_bootcamp/exercicio02.py
@@ -0,0 +1,8 @@
+# Crie um programa onde o usuário digita dois valores e apareça a soma
+
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+
+t = n1 + n2
+
+print(t)
