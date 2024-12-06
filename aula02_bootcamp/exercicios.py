92c8b6e48ac826e6be84c933ff879176f1fcc4a9
Robson Ganhara <robsongp@hotmail.com>
2024-12-06 16:04:00 -0300

    Exercício 01 da aula02

---
+++ aula02_bootcamp/exercicios.py
@@ -0,0 +1,8 @@
+# 1) Escreva um programa que soma dois números inteiros inserios pelo usuário
+
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+
+s = n1 + n2
+
+print(s)
