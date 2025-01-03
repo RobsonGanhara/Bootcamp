b6c43c99098e026c150b10e638a2ca9e67f1a9b0
Robson Ganhara <robsongp@hotmail.com>
2025-01-03 15:54:46 -0300

    Resolução dos exercícios 21 a 25

--- aula02_bootcamp/exercicios21.py
+++ aula02_bootcamp/exercicios21.py
@@ -1,2 +1,8 @@
 # 21: Conversor de Temperatura
 
+try:
+    celsius = float(input("Digite a temperatura em Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{celsius}°C é igual a {fahrenheit}°F.")
+except ValueError:
+    print("Por favor, digite um número válido para a temperatura.")
--- aula02_bootcamp/exercicios23.py
+++ aula02_bootcamp/exercicios23.py
@@ -1,2 +1,19 @@
 # 23: Calculadora Simples
 
+try:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    operador = input("Digite o operador (+, -, *, /): ")
+    if operador == '+':
+        resultado = num1 + num2
+    elif operador == '-':
+        resultado = num1 - num2
+    elif operador == '*':
+        resultado = num1 * num2
+    elif operador == '/' and num2 != 0:
+        resultado = num1 / num2
+    else:
+        print("Operador inválido ou divisão por zero.")
+    print("Resultado:", resultado)
+except ValueError:
+    print("Erro: Entrada inválida. Certifique-se de inserir números.")
